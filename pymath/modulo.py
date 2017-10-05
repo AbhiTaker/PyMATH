bd85d48516cf8bdb6ea27239d2091a7fd476f0a0
AbhiTaker <Abhi.taker20@gmail.com>
2017-10-05 09:24:40 +0530

    function instructio added in modulo module

--- pymath/modulo.py
+++ pymath/modulo.py
@@ -4,6 +4,7 @@ import math
 
 
 def add(num1, num2, modulo):
+    """ adding 2 numbers & taking their modulo """
 
     answer = ((num1 % modulo)+(num2 % modulo))%modulo
 
@@ -11,6 +12,7 @@ def add(num1, num2, modulo):
 
 
 def multiply(num1, num2, modulo):
+    """ multiplying 2 numbers & taking their modulo """
 
     answer = ((num1 % modulo)*(num2 % modulo))%modulo
     
@@ -18,6 +20,7 @@ def multiply(num1, num2, modulo):
 
 
 def sub(num1, num2, modulo):
+    """ subtracting 2 numbers & taking their modulo """
 
     answer = ( (num1 % modulo) - (num2 % modulo) + modulo )%modulo
 
