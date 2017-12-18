0cebed81d8f2395ccafd777ee02559431f26d3eb
AbhiTaker <Abhi.taker20@gmail.com>
2017-12-18 22:26:25 +0530

    div() added in modulo module

--- pymath/modulo.py
+++ pymath/modulo.py
@@ -3,18 +3,28 @@
 import math
 
 
-def add(num1, num2, modulo):
+def modInverse(num, mod):
+    """ function to return multiplicative modulo inverse of num & mod """
+
+    num = num % mod
+
+    for i in range(1, mod):
+        if (num * i) % mod == 1:
+            return i
+
+
+def add(num1, num2, mod):
     """ adding 2 numbers & taking their modulo """
 
-    answer = ((num1 % modulo)+(num2 % modulo))%modulo
+    answer = ((num1 % mod)+(num2 % mod))%mod
 
     return answer
 
 
-def multiply(num1, num2, modulo):
+def multiply(num1, num2, mod):
     """ multiplying 2 numbers & taking their modulo """
 
-    answer = ((num1 % modulo)*(num2 % modulo))%modulo
+    answer = ((num1 % mod)*(num2 % mod))%mod
     
     return answer
 
@@ -22,8 +32,17 @@ def multiply(num1, num2, modulo):
 def sub(num1, num2, modulo):
     """ subtracting 2 numbers & taking their modulo """
 
-    answer = ( (num1 % modulo) - (num2 % modulo) + modulo )%modulo
+    answer = ( (num1 % mod) - (num2 % mod) + mod )%mod
 
     return answer
 
 
+def div(num1, num2, mod):
+    """ function to divide two numbers and taking their modulo """
+
+    inv_num2 =  modInverse(num2, mod)                               #multiplicative modulo inverse of num2 and modulo 
+
+    answer = ( (num1 % mod)*(inv_num2 % mod) )%mod
+
+
+print(modInverse(5,12))
